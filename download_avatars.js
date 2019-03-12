b2ae7f9fa642fbe0a3fb1b203639b1e7d1354898
Ryota Mine <ryotamine@gmail.com>
2019-03-12 00:24:49 +0000

    step 4: making the request

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,10 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 // call getRepoContributors function
